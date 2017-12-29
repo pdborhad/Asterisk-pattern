c6d51f3606ea47149ba8d3d9eb14bc21e8860320
Prashant Borhade <32273989+pdborhad@users.noreply.github.com>
2017-12-29 01:19:19 -0600

    Update createPattern.cpp

--- createPattern.cpp
+++ createPattern.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
@@ -13,10 +5,7 @@ using namespace std;
 int
 main ()
 {
-    
-    char astr = '*';
     int no = 0;
-    
     do{
         cout << "Enter Odd Numberof Starts : ";
         cin >> no;
